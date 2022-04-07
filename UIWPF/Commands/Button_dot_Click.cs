fc6bc681066a449bcc9f3b3563e57f7ce8f4bb52
Marcin Frasz <noahy21@gmail.com>
2022-04-08 00:44:17 +0200

    Started implementation of Button_dot_Clicks.cs still need to finish case for sign_type='-'

--- UIWPF/Commands/Button_dot_Click.cs
+++ UIWPF/Commands/Button_dot_Click.cs
@@ -14,6 +14,39 @@ namespace UIWPF.Commands
         {
             _calculatorViewModel = calculatorViewModel;
         }
+        private string Dot_functionality(string textBox_content,char sign_type)
+        {
+            string[] subs = { "", "" };
+            if (sign_type != '-')
+            {
+                subs = textBox_content.Split(sign_type);
+                if (!subs[1].Contains('.'))
+                {
+                    if (subs[1].Length == 0)
+                    {
+                        textBox_content = textBox_content + "0.";
+                    }
+                    else
+                    {
+                        textBox_content = textBox_content + ".";
+                    }
+
+                }
+            }
+            else
+            {
+                switch(textBox_content.Count(x=>x=='-'))
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+            return textBox_content;
+        }
         public override void Execute(object? parameter)
         {
             if(_calculatorViewModel.TextBlock_result.Contains("."))
@@ -22,24 +55,17 @@ namespace UIWPF.Commands
                 switch(_calculatorViewModel.TextBlock_result)
                 {
                     case String a when a.Contains("+"):
-                       
-                        string[] subs = _calculatorViewModel.TextBlock_result.Split('+');
-                        if(!subs[1].Contains('.'))
-                        {
-                            if(subs[1].Length==0)
-                            {
-                                _calculatorViewModel.TextBlock_result = _calculatorViewModel.TextBlock_result + "0.";
-                            }
-                            else
-                            {
-                                _calculatorViewModel.TextBlock_result = _calculatorViewModel.TextBlock_result + ".";
-                            }
-                            
-                        }
+                        _calculatorViewModel.TextBlock_result=Dot_functionality(_calculatorViewModel.TextBlock_result, '+');
                         break;
-                    case String b when b.Contains("-"): _calculatorViewModel.TextBlock_result = "test-"; break;
-                    case String c when c.Contains("x"): _calculatorViewModel.TextBlock_result = "testx"; break;
-                    case String d when d.Contains("÷"): _calculatorViewModel.TextBlock_result = "test÷"; break;
+                    case String b when b.Contains("x"):
+                        _calculatorViewModel.TextBlock_result = Dot_functionality(_calculatorViewModel.TextBlock_result, 'x');
+                        break;
+                    case String c when c.Contains("÷"):
+                        _calculatorViewModel.TextBlock_result = Dot_functionality(_calculatorViewModel.TextBlock_result, '÷');
+                        break;
+                    case String d when d.Contains("-"):
+                        _calculatorViewModel.TextBlock_result = Dot_functionality(_calculatorViewModel.TextBlock_result, '-');
+                        break;
                 }               
             }
             else
